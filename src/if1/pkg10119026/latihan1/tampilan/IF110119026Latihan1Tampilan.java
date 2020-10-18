09ff0ae5f766f7f812f3a19714dc894f3bd81e81
mkhatami34 <koabarab361@gmail.com>
2020-10-18 19:53:39 +0700

    initial commit

---
+++ src/if1/pkg10119026/latihan1/tampilan/IF110119026Latihan1Tampilan.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10119026.latihan1.tampilan;
+
+/**
+ *
+ * @author Asus
+ * NAMA     : Muhammad Khatami
+ * KELAS    : PBO
+ * NIM      : 10119026
+ * Deskripsi Program : Program ini berisi program untuk menampilkan
+ * value ke layar
+ */
+public class IF110119026Latihan1Tampilan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println("BIODATA ROCKSTAR"+"\n"+"---------------------");
+        System.out.println("NAMA\t\t: Muhammad Khatami");
+        System.out.println("ALAMAT\t\t: Jalan Maleber Utara No 151");
+        System.out.println("TEMPAT LAHIR\t: Bandung");
+        System.out.println("TANGGAL LAHIR\t: 7 September 2000");
+        System.out.println("---------------------");
+        System.out.println("HOBI\t\t: Sepak Bola");
+        System.out.println("MAKES\t\t: Sayur Tahu");
+        System.out.println("MIKES\t\t: Cendol");
+    }
+}
